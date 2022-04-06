ff3d487289ec108177725debe48ccc7d581cce8b
Afonso Praça <afonsoinfo@gmail.com>
2022-04-06 18:12:08 -0300

    content adjusted

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,12 @@ const headingStyles = {
   marginBottom: 64,
   maxWidth: 320,
 }
+const heading1Styles = {
+  color: "black"
+}
 const headingAccentStyles = {
   color: "#663399",
+  fontSize: "1.5rem"
 }
 const paragraphStyles = {
   marginBottom: 48,
@@ -33,6 +37,14 @@ const listItemStyles = {
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
+  color: "#000",
+}
+
+const roleStyles = {
+  fontWeight: 300,
+  fontSize: 14,
+  fontWeight: "bold",
+  color: "#A0A0A0"
 }
 
 const linkStyle = {
@@ -78,50 +90,27 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// data
-const links = [
+const experiences = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    company: "VTEX",
+    companyUrl: "https://vtex.com/us-en/",
+    role: "Senior Software Developer [Team Lead]",
+    from: "July 2012",
+    to: "Present"
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    company: "Caos! Design",
+    companyUrl: "http://caosdesign.com.br/",
+    role: "Business Partner and Tech Lead",
+    from: "June 2009",
+    to: "June 2012"
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
+    company: "Ceperj",
+    companyUrl: "https://www.ceperj.rj.gov.br/",
+    role: "System Administrator",
+    from: "May 2008",
+    to: "May 2009"
   },
 ]
 
@@ -129,54 +118,32 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title>Afonso Praça</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Afonso Praça
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+        <span style={headingAccentStyles}>building software and businesses</span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
+      <h2 style={heading1Styles}>Experience</h2>
       <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+        {experiences.map(experience => (
+          <li key={experience.companyUrl} style={{ ...listItemStyles}}>
             <span>
               <a
                 style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                href={`${experience.companyUrl}?utm_source=afonsopraca&utm_medium=start-page&utm_campaign=afonsopraca`}
+                target="_blank"
               >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
+                {experience.company}
+              </a> - <span style={{ ...roleStyles}}>{experience.role}</span>
+              <p style={descriptionStyle}>{experience.from} - {experience.to}</p>
             </span>
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <h2 style={heading1Styles}>Contacts</h2>
+      <span>+55 21 979 151515</span><br/>
+      <span>afonsoinfo@gmail.com</span>
     </main>
   )
 }
